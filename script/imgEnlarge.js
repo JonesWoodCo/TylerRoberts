48276da829e9d49908a2231e0b714d0594377376
Topher <cjones@sycamoreleaf.com>
2015-03-27 16:56:06 -0500

    image light box work.

--- script/imgEnlarge.js
+++ script/imgEnlarge.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-    $(".column, .enlarge").imageLightbox({});
+    $(".column, .enlarge").imageLightbox({
+        quitOnEnd:      true,
+        quitOnImgClick: true
+        });
     });
+
+// image light box plugin can be found here. Thanks Osvaldas Valutis
+// http://osvaldas.info/image-lightbox-responsive-touch-friendly
